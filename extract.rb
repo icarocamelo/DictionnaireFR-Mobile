5ecca8f96cfa36e377c2e28c6fdab73b799e3577
Icaro Camelo <icarorvc@gmail.com>
2014-01-09 14:51:22 -0300

    Added a new datasource.

--- extract.rb
+++ extract.rb
@@ -6,3 +6,5 @@ value = ARGV[0]
 page = Nokogiri::HTML(open("http://www.le-dictionnaire.com/definition.php?mot=" + value))
 puts page.xpath("//td[@class='arial-12-gris']")
 #puts page.css('td.arial-12-gris')
+
+#Another sourche: http://dictionary.reverso.net/french-definition/maison
