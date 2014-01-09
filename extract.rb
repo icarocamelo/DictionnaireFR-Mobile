2bd874c6061e91b4c28c693fbb933796616b88d8
Icaro Camelo <icarorvc@gmail.com>
2014-01-09 14:33:19 -0300

    Added dynamic search by word.

--- extract.rb
+++ extract.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-page = Nokogiri::HTML(open("http://www.le-dictionnaire.com/definition.php?mot=maison"))
+value = ARGV[0]
+page = Nokogiri::HTML(open("http://www.le-dictionnaire.com/definition.php?mot=" + value))
 puts page.xpath("//td[@class='arial-12-gris']")
 #puts page.css('td.arial-12-gris')
