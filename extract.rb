6d9839fd70e36eb3b8b360c3fd766d33fef2364e
Icaro Camelo <icarorvc@gmail.com>
2014-01-09 15:01:52 -0300

    Added details about new datasource.

--- extract.rb
+++ extract.rb
@@ -3,8 +3,12 @@ require 'nokogiri'
 require 'open-uri'
 
 value = ARGV[0]
-page = Nokogiri::HTML(open("http://www.le-dictionnaire.com/definition.php?mot=" + value))
-puts page.xpath("//td[@class='arial-12-gris']")
+link1 = Nokogiri::HTML(open("http://www.le-dictionnaire.com/definition.php?mot=" + value))
+#link2 = Nokogiri::HTML(open("http://dictionary.reverso.net/french-definition/" + value))
+
+puts link1.xpath("//td[@class='arial-12-gris']")
+#puts link2.xpath("//div[@id='TableHTMLResult']")
 #puts page.css('td.arial-12-gris')
 
 #Another sourche: http://dictionary.reverso.net/french-definition/maison
+#div id: TableHTMLResult
