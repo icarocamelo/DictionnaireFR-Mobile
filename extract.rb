2028f14cf1c4f58ba7c279dbdbc1d6549a75d35f
Icaro Camelo <icarorvc@gmail.com>
2014-08-11 15:37:43 -0400

    Removed spans

--- extract.rb
+++ extract.rb
@@ -5,6 +5,5 @@ require 'open-uri'
 value = ARGV[0]
 link = Nokogiri::HTML(open("http://www.laparlure.com/terme/" + value))
 result = link.xpath("//div[@class='grid_9']/table/*[not(span)]")
-#result = link.search("//span").remove
-#doc2.xpath('//img').each {|x| x.remove}
+result.search("//span").remove
 puts result.to_s.encode("iso-8859-1").force_encoding("utf-8")
