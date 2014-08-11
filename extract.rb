0a7f59d937e05b5e3f82c29e47891a4518b60ee8
Icaro Camelo <icarorvc@gmail.com>
2014-08-11 16:00:40 -0400

    Removing p and divs

--- extract.rb
+++ extract.rb
@@ -6,4 +6,6 @@ value = ARGV[0]
 link = Nokogiri::HTML(open("http://www.laparlure.com/terme/" + value))
 result = link.xpath("//div[@class='grid_9']/table/*[not(span)]")
 result.search("//span").remove
+result.search("//p").remove
+result.search("//div").remove
 puts result.to_s.encode("iso-8859-1").force_encoding("utf-8")
