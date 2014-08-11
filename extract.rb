ee6e1ad4607d4a908240148651a52a41e24c393f
Icaro Camelo <icarorvc@gmail.com>
2014-08-11 15:14:54 -0400

    Formatting and encoding response

--- extract.rb
+++ extract.rb
@@ -3,14 +3,8 @@ require 'nokogiri'
 require 'open-uri'
 
 value = ARGV[0]
-link1 = Nokogiri::HTML(open("http://www.le-dictionnaire.com/definition.php?mot=" + value))
-#link2 = Nokogiri::HTML(open("http://dictionary.reverso.net/french-definition/" + value))
-#link3 = Nokogiri::HTML(open("http://www.laparlure.com/terme/" + value))
-
-puts link1.xpath("//td[@class='arial-12-gris']")
-#puts link2.xpath("//div[@id='TableHTMLResult']")
-#puts link3.xpath("//div[@id='grid_9']")
-#puts page.css('td.arial-12-gris')
-
-#Another sourche: http://dictionary.reverso.net/french-definition/maison
-#div id: TableHTMLResult
+link = Nokogiri::HTML(open("http://www.laparlure.com/terme/" + value))
+result = link.xpath("//div[@class='grid_9']/table/*[not(span)]")
+#result = link.search("//span").remove
+#doc2.xpath('//img').each {|x| x.remove}
+puts result.to_s.encode("iso-8859-1").force_encoding("utf-8")
