7d03cdfb5587874ac38788f0f67963067ffc4c70
Icaro Camelo <icarorvc@gmail.com>
2014-08-11 10:25:29 -0400

    Added new dictionary link

--- extract.rb
+++ extract.rb
@@ -5,9 +5,11 @@ require 'open-uri'
 value = ARGV[0]
 link1 = Nokogiri::HTML(open("http://www.le-dictionnaire.com/definition.php?mot=" + value))
 #link2 = Nokogiri::HTML(open("http://dictionary.reverso.net/french-definition/" + value))
+#link3 = Nokogiri::HTML(open("http://www.laparlure.com/terme/" + value))
 
 puts link1.xpath("//td[@class='arial-12-gris']")
 #puts link2.xpath("//div[@id='TableHTMLResult']")
+#puts link3.xpath("//div[@id='grid_9']")
 #puts page.css('td.arial-12-gris')
 
 #Another sourche: http://dictionary.reverso.net/french-definition/maison
