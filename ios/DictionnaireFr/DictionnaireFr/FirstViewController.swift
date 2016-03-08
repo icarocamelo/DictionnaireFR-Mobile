6fc97884cb72217aea186954e537ca1bafd1435f
Icaro Camelo <icamelo@inocybe.com>
2016-03-08 00:50:41 -0500

    New template version

---
+++ ios/DictionnaireFr/DictionnaireFr/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DictionnaireFr
+//
+//  Created by IcaroCamelo on 2016-03-08.
+//  Copyright © 2016 IcaroCamelo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
